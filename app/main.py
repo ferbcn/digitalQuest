f0bca02e355790b70017975f5f6c7620fd61a5f2
garcifer <garcifer@students.zhaw.ch>
2023-10-08 00:36:44 +0200

    simple echo terminal

--- app/main.py
+++ app/main.py
@@ -1,21 +1,62 @@
 import time
 import json
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, RedirectResponse, FileResponse, JSONResponse
+from fastapi import FastAPI, Request, WebSocket
+from fastapi.responses import HTMLResponse
 
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
-app = FastAPI(title='WebsocketAPI')
+from typing import List
+
+from starlette.websockets import WebSocketDisconnect
+
+app = FastAPI(title='TheDigitalQuest')
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, data: str):
+        for connection in self.active_connections:
+            #print("Sendig JSON data:", data)
+            try:
+                await connection.send_text(data)
+            except WebSocketDisconnect:
+                self.disconnect(connection)
+
+manager = ConnectionManager()
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
     current_level = 0
     return templates.TemplateResponse("index.html", {"request": request, "level": current_level})
+
+# Websocket endpoint
+@app.websocket("/terminalws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        # await for messages and send messages
+        while True:
+            data = await websocket.receive_text()
+            if data == "ping":
+                await websocket.send_text("pong")
+            else:
+                await websocket.send_text(data)
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
 
 
 if __name__ == "__main__":
--- app/static/index.js
+++ app/static/index.js
@@ -1,1 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Your entire JavaScript code here
 
+    let terminal = document.getElementById("terminal");
+
+    let ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+    let wsUrl = ws_scheme + '://' + window.location.host + "/terminalws"
+    let socket = new WebSocket(wsUrl);
+
+    socket.onmessage = (event) => {
+        console.log(event.data);
+        terminal.innerText += "> " + event.data + '\r';
+        terminal.scrollTop = terminal.scrollHeight;
+    };
+
+    socket.onclose = function(event) {
+        terminal.innerText = 'Connection lost';
+    };
+
+    socket.onerror = function(error) {
+        terminal.innerText = `Error: ${error.message}`;
+    };
+
+    socket.onopen = function(event) {
+        terminal.innerText = 'Connection established!' + '\r';
+        socket.send("ping");
+    };
+
+    let command = "";
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            terminal.innerText += '\r';
+            socket.send(command);
+            command = "";
+        }
+        else{
+            const key = event.key;
+            command += key;
+            terminal.innerText += key;
+        }
+    });
+
+});
