aae7abca313d25315f3b77b54699a280231cd69f
garcifer <garcifer@students.zhaw.ch>
2023-10-10 16:48:40 +0200

    cleaner code and session class added

--- app/main.py
+++ app/main.py
@@ -17,21 +17,6 @@ app = FastAPI(title='TheDigitalQuest')
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-dir_tree = {
-    "/": {"parent_dir": "/", "content": [{"name": "file1", "type": "file"}, {"name": "dir1", "type": "dir"}]},
-    "dir1": {"parent_dir": "/", "content": [{"name": "file2", "type": "file"}, {"name": "file3", "type": "file"},
-                                            {"name": "dir2", "type": "dir"}]},
-    "dir2": {"parent_dir": "dir1", "content": [{"name": "file4", "type": "file"}, {"name": "file5", "type": "file"}]}
-}
-
-files = {
-    "file1": {"content": {"data": "This is a test: file1!", "owner": "root"}},
-    "file2": {"content": {"data": "This is a test: file2!", "owner": "guest"}},
-    "file3": {"content": {"data": "This is a test: file3!", "owner": "root"}},
-    "file4": {"content": {"data": "This is a test: file4!", "owner": "root"}},
-    "file5": {"content": {"data": "This is a test: file5!", "owner": "root"}},
-}
-
 
 class ConnectionManager:
     def __init__(self):
@@ -52,7 +37,55 @@ class ConnectionManager:
             except WebSocketDisconnect:
                 self.disconnect(connection)
 
-manager = ConnectionManager()
+class Session:
+    def __init__(self):
+        self.dir_tree = {
+    "/": {"parent_dir": "/", "content":
+        [{"name": "file1", "type": "file"},
+         {"name": "dir1", "type": "dir"}]},
+    "dir1": {"parent_dir": "/", "content": [{"name": "file2", "type": "file"}, {"name": "file3", "type": "file"},
+                                            {"name": "dir2", "type": "dir"}]},
+    "dir2": {"parent_dir": "dir1", "content": [{"name": "file4", "type": "file"}, {"name": "file5", "type": "file"}]}
+    }
+
+        self.files = {
+    "file1": {"content": {"data": "This is a test: file1! This is a test: file1! This is a test: file1! "
+                                  "This is a test: file1!", "owner": "root"}},
+    "file2": {"content": {"data": "This is a test: file2!", "owner": "guest"}},
+    "file3": {"content": {"data": "This is a test: file3!", "owner": "root"}},
+    "file4": {"content": {"data": "This is a test: file4!", "owner": "root"}},
+    "file5": {"content": {"data": "This is a test: file5!", "owner": "root"}},
+}
+
+        self.current_dir = "/"
+        self.current_user = "root"
+        self.available_dirs = []
+        self.available_files = []
+    def get_all(self):
+        output = ""
+        for item in self.dir_tree.get(self.current_dir).get("content"):
+            output += item.get("name") + " / " + item.get("type") + "\n"
+        return output
+    def get_dirs(self):
+        available_dirs = [item.get("name") for item in self.dir_tree.get(self.current_dir).get("content") if
+                          item.get("type") == "dir"]
+        available_dirs.append(self.dir_tree.get(self.current_dir).get("parent_dir"))
+        available_dirs.append("/")
+        return available_dirs
+
+    def get_files(self):
+        available_files = [item.get("name") for item in self.dir_tree.get(self.current_dir).get("content") if
+                           item.get("type") == "file"]
+        return available_files
+
+    def get_parent_dir(self):
+        return self.dir_tree.get(self.current_dir).get("parent_dir")
+
+    def set_current_dir(self, dir):
+        self.current_dir = dir
+        self.available_dirs = self.get_dirs()
+        self.available_files = self.get_files()
+
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
@@ -63,78 +96,68 @@ def index(request: Request):
 # Websocket endpoint
 @app.websocket("/terminalws")
 async def websocket_endpoint(websocket: WebSocket):
-    current_dir = "/"
-    current_user = "root"
-    available_dirs = [item.get("name") for item in dir_tree.get(current_dir).get("content") if item.get("type") == "dir"]
-    available_dirs.append(dir_tree.get(current_dir).get("parent_dir"))
-    available_files = [item.get("name") for item in dir_tree.get(current_dir).get("content") if item.get("type") == "file"]
-
     await manager.connect(websocket)
     try:
+        #available_dirs = session.get_dirs()
+        #available_files = session.get_files()
+        session.set_current_dir("/")
         # await for messages and send messages
         while True:
             data = await websocket.receive_text()
-            if data == "ping":
-                await websocket.send_text("pong")
-            elif data == "clear":
+            command_list = data.split(" ")
+            # Test massages
+            if command_list[0] == "ping":
+                await websocket.send_text("pong\n")
+            elif command_list[0] == "hello":
+                await websocket.send_text("world!\n")
+
+            # Basic commands
+            elif command_list[0] == "clear":
                 await websocket.send_text("clear")
-            elif data == "help":
-                available_commands = ["help", "clear", "exit", "ls", "cwd", "cd [dir]", "cd..", "cat"]
+            elif command_list[0] == "help":
+                available_commands = ["help", "clear", "exit", "ls", "cwd", "cd [dir]", "cat [file]"]
                 command_list_str = "Available commands:\n" + "\n".join([com for com in available_commands]) + "\n"
                 await websocket.send_text(command_list_str)
-            elif data == "hello":
-                await websocket.send_text("world!\n")
-            elif data == "exit":
+            elif command_list[0] == "exit":
                 await websocket.send_text("exit")
-            elif data == "ls":
-                output = f"Directory content of {current_dir}: \n" \
+            elif command_list[0] == "ls":
+                output = f"Directory content of {session.current_dir}: \n" \
                          "Name / Type: \n"
-                for item in dir_tree.get(current_dir).get("content"):
-                    output += item.get("name") + " / " + item.get("type") + "\n"
+                output += session.get_all()
                 await websocket.send_text(output)
-            elif data[:3] == "cd":
-                target_dir = "/"
-                current_dir = target_dir
-                available_dirs = [item.get("name") for item in dir_tree.get(current_dir).get("content") if
-                                  item.get("type") == "dir"]
-                available_dirs.append(dir_tree.get(current_dir).get("parent_dir"))
-                available_files = [item.get("name") for item in dir_tree.get(current_dir).get("content") if
-                                   item.get("type") == "file"]
-                await websocket.send_text(f"Changed to: {target_dir} \n")
-            elif data == "cd ..":
-                current_dir = dir_tree.get(current_dir).get("parent_dir")
-                available_dirs = [item.get("name") for item in dir_tree.get(current_dir).get("content") if
-                                  item.get("type") == "dir"]
-                available_dirs.append(dir_tree.get(current_dir).get("parent_dir"))
-                available_files = [item.get("name") for item in dir_tree.get(current_dir).get("content") if
-                                   item.get("type") == "file"]
-                await websocket.send_text(f"Changed to: {current_dir} \n")
-            elif data[:3] == "cd ":
-                target_dir = data[3:]
-                if target_dir in available_dirs:
-                    current_dir = target_dir
-                    available_dirs = [item.get("name") for item in dir_tree.get(current_dir).get("content") if item.get("type") == "dir"]
-                    available_dirs.append(dir_tree.get(current_dir).get("parent_dir"))
-                    available_files = [item.get("name") for item in dir_tree.get(current_dir).get("content") if
-                                       item.get("type") == "file"]
+            elif command_list[0] == "cd":
+                if len(command_list) > 1:
+                    if command_list[1] == "..":
+                        target_dir = session.get_parent_dir()
+                    else:
+                        target_dir = command_list[1]
+                else:
+                    target_dir = "/"
+
+                if target_dir in session.available_dirs:
+                    session.set_current_dir(target_dir)
                     await websocket.send_text(f"Changed to: {target_dir} \n")
                 else:
                     await websocket.send_text(f"{target_dir} not found or not a directory!\n")
 
-            elif data[:4] == "cat ":
-                target_file = data[4:]
-                if target_file in available_files:
-                    file = files.get(target_file).get("content")
-                    if file.get("owner") == current_user:
+            elif data == "cwd":
+                await websocket.send_text(f"Current dir: {session.current_dir}\n")
+
+            # Advanced commands
+            elif command_list[0] == "cat" and len(command_list) > 1:
+                target_file = command_list[1]
+                if target_file in session.available_files:
+                    file = session.files.get(target_file).get("content")
+                    if file.get("owner") == session.current_user:
                         await websocket.send_text(file.get("data") + "\n")
                     else:
                         await websocket.send_text("Not authorized! \n")
                 else:
                     await websocket.send_text(f"{target_file} not found!\n")
 
-
-            elif data == "cwd":
-                await websocket.send_text(f"Current dir: {current_dir}\n")
+            # Non commands
+            elif command_list[0] == "":
+                await websocket.send_text(f"")
 
             else:
                 await websocket.send_text("Unknown command!\n")
@@ -144,4 +167,6 @@ async def websocket_endpoint(websocket: WebSocket):
 
 
 if __name__ == "__main__":
+    manager = ConnectionManager()
+    session = Session()
     uvicorn.run(app, host="0.0.0.0", port=8000)
