c540389138f7cf045b24402200e165953ac0ed15
garcifer <garcifer@students.zhaw.ch>
2023-10-25 17:32:13 +0200

    major clean-up

--- app/main.py
+++ app/main.py
@@ -44,71 +44,25 @@ class ConnectionManager:
                 self.disconnect(connection)
 
 
-manager = ConnectionManager()
-
-
-class FileSystem:
-    def __init__(self):
-        with open("static/dir_tree.json", "r") as dir_json:
-            self.dir_tree = json.load(dir_json)
-        with open("static/files.json", "r") as files_json:
-            self.files = json.load(files_json)
-        self.current_dir = "/"
-        self.current_user = "root"
-        self.available_dirs = []
-        self.available_files = []
-        self.command_history = []
-
-        self.command_list = {}
-
-    def get_all(self):
-        output = ""
-        for item in self.dir_tree.get(self.current_dir).get("content"):
-            output += item.get("name") + " / " + item.get("type") + "\n"
-        return output
-
-    def get_dirs(self):
-        available_dirs = [item.get("name") for item in self.dir_tree.get(self.current_dir).get("content") if
-                          item.get("type") == "dir"]
-        available_dirs.append(self.dir_tree.get(self.current_dir).get("parent_dir"))
-        available_dirs.append("/")
-        return available_dirs
-
-    def get_files(self):
-        available_files = [item.get("name") for item in self.dir_tree.get(self.current_dir).get("content") if
-                           item.get("type") == "file"]
-        return available_files
-
-    def get_parent_dir(self):
-        return self.dir_tree.get(self.current_dir).get("parent_dir")
-
-    def set_current_dir(self, dir):
-        self.current_dir = dir
-        self.available_dirs = self.get_dirs()
-        self.available_files = self.get_files()
-
-
-my_system = FileSystem()
-
-
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
     current_level = 0
     return templates.TemplateResponse("index.html", {"request": request, "level": current_level})
 
+
 @app.get("/rain", response_class=HTMLResponse)
 def index(request: Request):
-    current_level = 0
     return templates.TemplateResponse("rain.html", {"request": request})
+
 
 # Websocket endpoint
 @app.websocket("/wsconsole")
 async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
     try:
-        my_system.set_current_dir("/")
         command_buffer = ""
         command_history_pos = 1
+        command_history = []
         # await for messages and send messages
         while True:
             last_data = await websocket.receive_text()
@@ -124,27 +78,27 @@ async def websocket_endpoint(websocket: WebSocket):
             elif last_data == "<*bck*>" or last_data == "<*fwd*>":
                 mes_type = "history"
                 if last_data == "<*bck*>":
-                    if len(my_system.command_history) > 0:
-                        command_buffer = my_system.command_history[-command_history_pos]
-                    if len(my_system.command_history) > command_history_pos:
+                    if len(command_history) > 0:
+                        command_buffer = command_history[-command_history_pos]
+                    if len(command_history) > command_history_pos:
                         command_history_pos += 1
                 elif last_data == "<*fwd*>":
                     if command_history_pos > 1:
                         command_history_pos -= 1
                     if command_history_pos >= 1:
-                        command_buffer = my_system.command_history[-command_history_pos]
+                        command_buffer = command_history[-command_history_pos]
 
                 mes_object = {"type": mes_type, "content": command_buffer}
-                print("History:", my_system.command_history, command_buffer)
+                print("History:", command_history, command_buffer)
 
             # Process new command after newline
             elif last_data == "\n":
                 if len(command_buffer) > 0:
-                    my_system.command_history.append(command_buffer)
+                    command_history.append(command_buffer)
                     command_history_pos = 1
                     command_list = command_buffer.split(" ")
                     command_buffer = ""
-                    mes_object = await process_command(websocket, command_list)
+                    mes_object = await process_command(command_list)
                 else:
                     mes_object = {"type": "text", "content": "\n$"}
 
@@ -160,9 +114,9 @@ async def websocket_endpoint(websocket: WebSocket):
         manager.disconnect(websocket)
 
 
-async def process_command(websocket, command_list):
-    print("Commands and args: ", command_list)
-    mes_type = "text" # default message type
+async def process_command(command_list):
+    print("Command and args: ", command_list)
+    mes_type = "text"   # default message type
 
     if command_list[0] == "hello":
         # await websocket.send_text("\nworld!\n$")
@@ -179,7 +133,7 @@ async def process_command(websocket, command_list):
         mes_type = "text"
 
     elif command_list[0] == "exit":
-        my_system.command_history = []
+        # command_history = []
         mes_type = "system"
         mes_content = "exit"
 
@@ -188,7 +142,7 @@ async def process_command(websocket, command_list):
         if len(command_list) > 1:
             filename = command_list[1]
         try:
-            filepath = "static/" + filename
+            filepath = "static/files/" + filename
             print(filepath)
             with open(filepath, "r") as text_file:
                 mes_content = "\n"
@@ -231,14 +185,6 @@ async def process_command(websocket, command_list):
     return {"type": mes_type, "content": mes_content}
 
 
-def calc_fib(num):
-    if num <= 0:
-        return 0
-    elif num <= 1:
-        return 1
-    else:
-        return calc_fib(num-1) + calc_fib(num-2)
-
-
 if __name__ == "__main__":
+    manager = ConnectionManager()
     uvicorn.run(app, host="127.0.0.1", port=8000)
